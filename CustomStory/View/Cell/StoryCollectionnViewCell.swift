6cba8dc837b5ce20a184773d86fd456fdbd8f5cc
Arif-Amzad <arifamzad189@gmail.com>
2021-06-12 16:52:34 +0600

    first commit

---
+++ CustomStory/View/Cell/StoryCollectionnViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  StoryCollectionnViewCell.swift
+//  CustomStory
+//
+//  Created by Arif Amzad on 11/6/21.
+//
+
+import UIKit
+
+class StoryCollectionnViewCell: UICollectionViewCell {
+    
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var storyImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.size.width/2
+        avatarImageView.layer.borderColor = UIColor.systemBlue.cgColor
+        avatarImageView.layer.borderWidth = 3
+        
+        storyImageView.layer.cornerRadius = 8
+    }
+
+}
